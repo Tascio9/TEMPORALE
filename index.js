a169d5f923383be06cdb5ef0700e507c835049e5
Tascio9 <g.tasciotti95@gmail.com>
2021-02-23 19:35:47 +0100

    Added popup on "Settings" and fixed the CSS.

--- index.js
+++ index.js
@@ -129,13 +129,29 @@ Promise.all([
   })
 
   // - Button SETTINGS
-  d3.select('#settings').on('click', function() {
-    window.alert('Settings')
+  d3.select('#settings').on('click', function () {
+    var modal = document.getElementById("myModal");
+
+    modal.style.display = "block";
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   })
 
   // - Button ABOUT
-   d3.select('#about').on('click', function() {
-     window.alert('About')
+  d3.select('#about').on('click', function () {
+    window.alert('About')
   })
 
   // - Button RESET
